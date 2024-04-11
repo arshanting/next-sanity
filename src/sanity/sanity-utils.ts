f978d378b6998f857047c4884246a9a3db688282
arshanting <arshanting@gmail.com>
2024-04-12 00:23:50 +0800

    Created a schema for pages

--- src/sanity/sanity-utils.ts
+++ src/sanity/sanity-utils.ts
@@ -1,6 +1,7 @@
 import { Project } from "@/types/Project";
 import { createClient, groq } from "next-sanity";
 import client from "./config/client-config";
+import { Page } from "@/types/Page";
 
 export async function getProjects(): Promise<Project[]> {
   return createClient(client).fetch(
@@ -26,6 +27,31 @@ export async function getProject(slug: string): Promise<Project> {
       "slug": slug.current,
       "image": image.asset->url,
       url,
+      content
+    }`,
+    { slug }
+  )
+}
+
+export async function getPages(): Promise<Page[]> {
+  return createClient(client).fetch(
+    groq`*[_type == 'page']{
+      _id,
+      _createdAt,
+      name,
+      'slug': slug.current,
+    }`,
+    // { next: { revalidate: 100 }}
+  )
+}
+
+export async function getPage(slug: string): Promise<Page> {
+  return createClient(client).fetch(
+    groq`*[_type == "page" && slug.current == $slug][0]{
+      _id,
+      _createdAt,
+      name,
+      "slug": slug.current,
       content
     }`,
     { slug }
